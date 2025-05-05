eb2e907464a2d8357be2b9ba617cc3038d7d6e15
FranciscoGJR <franciscoogjr@gmail.com>
2025-05-05 16:49:47 -0300

    refactor: exibindo mensagem incorreta

--- src/main/java/dsid/peerToPeer/model/rede/ThreadComunicacao.java
+++ src/main/java/dsid/peerToPeer/model/rede/ThreadComunicacao.java
@@ -50,6 +50,7 @@ public class ThreadComunicacao implements Runnable{
     public void run() {
         try {
             Mensagem mensagemRecebida = receberMensagem();
+            System.out.println(mensagemRecebida.toString());
             caixaDeMensagens.adicionarMensagemRecebida(mensagemRecebida);
             No noOrigem = mensagemRecebida.getOrigem();
             noOrigem.getRede().setStatus(ONLINE);;
